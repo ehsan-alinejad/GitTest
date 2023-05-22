807c59df7a6523c1ff25ac455fe0ed44a3bdf2eb
ehsan ehsan <ehsan.alinejad256@gmail.com>
2023-05-22 14:41:57 +0330

    add lines

--- code1.py
+++ code1.py
@@ -2,6 +2,9 @@
 #to hamun folder ke mikhaim git ejra beshe cmd mizanin
 #git init
 #git status
-#git add .  or git add code1.py
+#git add .  or git add code1.py  har taghiir add hamun file bad commit kari ke kardim
 #git commit -m 'hi git'
-#
+#git log
+#git diff head
+#git diff --staged
+
